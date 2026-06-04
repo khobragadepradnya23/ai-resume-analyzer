ac7bca74ec885c1ac9e6df69a6b9d9ba445af424
Pradnya <khobragadepradnya23@gmail.com>
2026-06-04 23:20:13 +0530

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,9 @@ import Navbar from "~/components/Navbar";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
 import ScoreCircle from "~/components/ScoreCircle";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,6 +15,13 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const { auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      if (!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated]);
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
     <section className="main-section">
@@ -19,7 +29,7 @@ export default function Home() {
         <h1>Track Your Application & Resume Ratings</h1>
         <h2>Review your submission and check AI-powered feedback.</h2>
       </div>
-      
+
       {resumes && resumes.length > 0 && (
         <div className="resumes-section">
           {resumes.map( (resume) => (
